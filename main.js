80f881edb6e609ec82a84a37abe488d2781df347
Filippo Rossi <rossifilippo2000@gmail.com>
2021-11-08 18:09:05 +0100

    aggiunta soluzione che utilizza le classi

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
-const menuOpen = document.getElementById('openMenu');
+// soluzione con gli id
+// const menuOpen = document.getElementById('openMenu');
 
-menuOpen.addEventListener("click", function() {
-    document.getElementById("menu").style.display = "block";
+// menuOpen.addEventListener("click", function() {
+//     document.getElementById("menu").style.display = "block";
+// });
+
+// const menuClose = document.getElementById('closeMenu');
+
+// menuClose.addEventListener("click", function () {
+//     document.getElementById("menu").style.display = "none";
+// });
+
+
+
+
+// soluzione usando le classi
+const openMenu = document.querySelector(".fa-bars");
+openMenu.addEventListener("click", function () {
+    document.querySelector(".hamburger-menu").classList.add("active")
 });
 
-const menuClose = document.getElementById('closeMenu');
-
-menuClose.addEventListener("click", function () {
-    document.getElementById("menu").style.display = "none";
+const closeMenu = document.querySelector(".fa-times");
+closeMenu.addEventListener("click", function () {
+    document.querySelector(".hamburger-menu").classList.remove("active")
 });
